be23411a8d63740083c1e35ed74482c9597589ed
Adnan Javed <adnanjaved@Mac-Adnan-SE-447.local>
2021-07-19 13:35:53 +0500

    Database connection and folder sturucture is done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { ExpenseModule } from './expenses/expense.module';
+
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, ExpenseModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database/entities/sub_categories.entity.ts
@@ -0,0 +1,18 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+enum type { INCOME = 'INCOME', EXPENSE = 'EXPENSE' }
+@Entity()
+export class SubCategories {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column({ length: 32 })
+    name: string;
+    @Column('text')
+    type: type;
+    @Column()
+    categories_id: number;
+    @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)" })
+    public created_at: Date;
+    @UpdateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)", onUpdate: "CURRENT_TIMESTAMP(6)" })
+    public updated_at: Date;
+}
